5453c52134bb20d868052b2e05756093062dae19
Jonah D. Hadlock <jonah-hadlock@lambdastudents.com>
2020-08-26 02:00:38 -0600

    FINALLY figured out task5; used some of the stuff from task4 to help me

--- index.js
+++ index.js
@@ -115,12 +115,16 @@ Hint: You can use .splice() for this
 
 */
 
-function removeFlavorByName( /*code here*/ ) {
-
-    /*code here*/
-
+function removeFlavorByName(arr, name) {
+    for (let i = 0; i <= arr.length; i++) {
+        if (arr[i] === name) {
+            arr.splice(i, 1);
+        }
+    }
 }
+removeFlavorByName(originalFlavors, "Banana Nut Fudge");
 
+console.log(originalFlavors);
 
 /* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array. 
 
